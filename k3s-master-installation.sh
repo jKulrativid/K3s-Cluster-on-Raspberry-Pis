834705e9053c502379bf72aaa3ccb9479cfba4a5
K. Jetnipit <jetnipit54@gmail.com>
2023-11-03 01:17:01 +0700

    chore: add empty script for k8s master and worker installation

---
+++ k3s-master-installation.sh
@@ -0,0 +1,10 @@
+# Installation Script for K3s Worker Node
+# OS : Ubuntu
+
+# Update existing packages
+sudo apt update
+sudo apt upgrade -y
+
+# Install vim for debugging purpose
+sudo apt install vim
+
---
+++ k3s-worker-installation.sh
@@ -0,0 +1,10 @@
+# Installation Script for K3s Worker Node
+# OS : Raspberry Pi OS
+
+# Update existing packages
+sudo apt update
+sudo apt upgrade -y
+
+# Install vim for debugging purpose
+sudo apt install vim
+
