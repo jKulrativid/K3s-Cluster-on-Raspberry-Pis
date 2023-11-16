6daeefe60f5a1fcb8e9cf6b58908d7eb735d2364
K. Jetnipit <jetnipit54@gmail.com>
2023-11-16 17:12:00 +0700

    chore: update worker installation script

--- k3s-worker-installation.sh
+++ k3s-worker-installation.sh
@@ -8,3 +8,26 @@ sudo apt upgrade -y
 # Install vim for debugging purpose
 sudo apt install vim
 
+cat << EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+    
+cat << EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+
+sudo swapoff -a
+
+cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory
+
+sudo mkdir -p /etc/rancher/k3s
+
+sudo -i
+echo "apiVersion: kubelet.config.k8s.io/v1beta1
+kind: KubeletConfiguration
+shutdownGracePeriod: 30s
+shutdownGracePeriodCriticalPods: 10s" > /etc/rancher/k3s/kubelet.config
+exit
---
+++ setup-user.sh
@@ -0,0 +1,4 @@
+# Note that this command is interactive
+sudo adduser sds
+
+sudo adduser sds sudo
