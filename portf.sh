abf5d701632c3de529410c3d0328a5e32b2fb1d9
K. Jetnipit <jetnipit54@gmail.com>
2023-11-19 23:55:40 +0700

    chore: update port-forwarding script

--- portf.sh
+++ portf.sh
@@ -3,12 +3,27 @@
 while true
 do
     kubectl port-forward service/web-service 3000:3000 &
-    PID=$!  # Capture the process ID of the last backgrounded command
+    PID1=$!  # Capture the process ID of the last backgrounded command
+	kubectl port-forward service/gateway-service 8080:8080 &
+    PID2=$!  # Capture the process ID of the last backgrounded command
+	kubectl port-forward service/chef-service 50001:50001 &
+    PID3=$!  # Capture the process ID of the last backgrounded command
+	kubectl port-forward service/recipe-service 50002:50002 &
+    PID4=$!  # Capture the process ID of the last backgrounded command
+	kubectl port-forward service/review-service 50003:50003 &
+    PID5=$!  # Capture the process ID of the last backgrounded command
 
-    sleep 5  # Wait for 5 seconds before terminating the port-forward
+    sleep 20  # Wait for 5 seconds before terminating the port-forward
 
     # Kill the port-forward process to avoid multiple instances running concurrently
-    kill $PID >/dev/null 2>&1  # Suppress errors in case the process doesn't exist
-
-    wait $PID 2>/dev/null  # Wait for the process to finish (suppress output)
+    kill $PID1 >/dev/null 2>&1  # Suppress errors in case the process doesn't exist
+    wait $PID1 2>/dev/null  # Wait for the process to finish (suppress output)
+	kill $PID2 >/dev/null 2>&1  # Suppress errors in case the process doesn't exist
+    wait $PID2 2>/dev/null  # Wait for the process to finish (suppress output)
+	kill $PID3 >/dev/null 2>&1  # Suppress errors in case the process doesn't exist
+    wait $PID3 2>/dev/null  # Wait for the process to finish (suppress output)
+	kill $PID4 >/dev/null 2>&1  # Suppress errors in case the process doesn't exist
+    wait $PID4 2>/dev/null  # Wait for the process to finish (suppress output)
+	kill $PID5 >/dev/null 2>&1  # Suppress errors in case the process doesn't exist
+    wait $PID5 2>/dev/null  # Wait for the process to finish (suppress output)
 done
